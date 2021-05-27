199aaea0bbeff7db4f3384c71a11ad5309c6fd06
Tyler Boyd <tboyd.evan@gmail.com>
2021-05-26 19:38:25 -0500

    prepping for heroku for a third time

--- src/resources/app/app.service.ts
+++ src/resources/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  serverTest(): string {
+    return 'Testing... API Hit. Test Success';
   }
 }
